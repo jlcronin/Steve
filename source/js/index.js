1ba1d811009d4bd5e04a9dc192bc643167cf35ad
jlcronin <jlcronin@users.noreply.github.com>
2014-09-08 19:29:11 +0100

    Wait for scroll event to finish before showing/hiding the footer

--- source/js/index.js
+++ source/js/index.js
@@ -1,12 +1,16 @@
-var lastScroll = 0;
+var lastScroll = 0,
+	timeout;
 $(document).ready(function() {
 	$(document).scroll(function(event) {
-		var st = $(this).scrollTop();
-		if (st < lastScroll){
-			$('footer').removeClass('close');
-		} else {
-			$('footer').addClass('close');
-		}
-		lastScroll = st;
+		clearTimeout(timeout);
+		timeout = setTimeout(function() {
+			var st = $(this).scrollTop();
+			if (st < lastScroll){
+				$('footer').removeClass('close');
+			} else {
+				$('footer').addClass('close');
+			}
+			lastScroll = st;
+		}, 250);
 	});
 });
